5652801f0db9f6f5806b90f5f1a142fd17d8a48c
sachitanand <sachitanand@sach.local>
2016-08-24 21:46:35 +0530

    first

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sachitanand/Documents/workspace/django/sachitecom/ecommerce-2/src/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
