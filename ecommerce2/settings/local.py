6f8c1576bbc4f344bef80ee8434976a3bb15531b
sachitanand <sachitanand@sach.local>
2016-08-25 21:47:19 +0530

    local.py

--- ecommerce2/settings/local.py
+++ ecommerce2/settings/local.py
@@ -102,13 +102,17 @@ WSGI_APPLICATION = 'ecommerce2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': 'd9grku30tl3bmv', 'USER': 'zmiddasodcajys', 'PASSWORD': 'uHIARGzROaETR92oVUvoQcBWfp', 'HOST': 'ec2-54-227-250-80.compute-1.amazonaws.com', 'PORT': '5432', }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
